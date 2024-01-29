4818bf788c0e3155f046fe2286d91282c54f93ec
Markus Realica <markusrealica.developer@gmail.com>
2024-01-28 18:11:23 -0800

    chore(Button): :fire: Remove dist folder and add to the .gitignore

--- packages/Button/dist/cjs/types/Button.types.d.ts
+++
@@ -1,9 +0,0 @@
-import { MouseEventHandler } from "react";
-export interface ButtonProps {
-    text?: string;
-    primary?: boolean;
-    disabled?: boolean;
-    size?: "small" | "medium" | "large";
-    children: React.ReactNode;
-    onClick?: MouseEventHandler<HTMLButtonElement>;
-}
