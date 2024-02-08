b398bd1db79a441afd9476513e938932d0d742bc
Markus Realica <markusrealica.developer@gmail.com>
2024-02-07 22:00:06 -0800

    feat(storybook-web): :sparkles: Add a 'Playground' vite application to test components.

---
+++ packages/storybook-web/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@realim1/button";
+import "@realim1/button/dist/esm/Button.css";
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<Button label={"Hello"} />
+		</>
+	);
+}
+
+export default App;
